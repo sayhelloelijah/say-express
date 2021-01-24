efe0c2d7543128c99ac49ed745ef7693ceeb7c78
sayhelloelijah <say@helloelijah.com>
2021-01-23 20:55:38 -0600

    :fire: remove puppeteer test

--- app.test.js
+++ app.test.js
@@ -1,4 +1,3 @@
-const puppeteer = require("puppeteer");
 const sum = require("./app");
 
 test("should add numbers together", () => {
@@ -7,17 +6,4 @@ test("should add numbers together", () => {
 
 test("should output nothing", () => {
     expect(sum()).toBe(NaN);
-});
-
-test("should click around", async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 8000,
-        args: ["--window-size=1920,1080"],
-    });
-
-    const page = await browser.newPage();
-    await page.goTo("http://localhost:4200");
-
-    expect();
 });
